ef5e5771fa1230b5f1c503e9c6bba6fdf598cbd8
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2023-03-02 15:37:09 -0300

    config next

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from '../styles/global-styles';
 import { theme } from "../styles/theme";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
+      <GlobalStyles />
     </ThemeProvider>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <h1>Oii</h1>;
+import P from "prop-types";
+import { mapData } from "../api/map-data";
+import config from "../config";
+import Home from "../templates/Home";
+
+export default function Index({ data = null }) {
+  return <Home data={data} />;
 }
+
+export const getStaticProps = async () => {
+  const raw = await fetch(config.url + config.defaultSlug);
+  const json = await raw.json();
+  const data = mapData(json);
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+Index.propTypes = {
+  data: P.array,
+};
